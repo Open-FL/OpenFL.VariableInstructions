50440c53697e3c1b22d471913260cdd06df3e2dd
Tim Akermann <ti.akermann@gmail.com>
2020-10-05 11:52:48 +0200

    Code Cleanup

--- src/OpenFL.VariableInstructions/VariableInstructionsPlugin.cs
+++ src/OpenFL.VariableInstructions/VariableInstructionsPlugin.cs
@@ -18,50 +18,50 @@ namespace OpenFL.VariableInstructions
                                                                                 "Defines a variable in the local scope and assigns a value to it"
                                                                                );
             PluginHost.AddInstructionWithDefaultCreator<DefineGlobalVarFLInstruction>(
-                                                                                      "gdef",
-                                                                                      "DV",
-                                                                                      "Defines a variable in global scope and assigns a value to it"
-                                                                                     );
+                 "gdef",
+                 "DV",
+                 "Defines a variable in global scope and assigns a value to it"
+                );
             PluginHost.AddInstructionWithDefaultCreator<DecrementVarFLInstruction>(
-                                                                                   "dec",
-                                                                                   "D|DV|DD",
-                                                                                   "Decrements a variable by 1 if no arguments specified."
-                                                                                  );
+                 "dec",
+                 "D|DV|DD",
+                 "Decrements a variable by 1 if no arguments specified."
+                );
             PluginHost.AddInstructionWithDefaultCreator<IncrementVarFLInstruction>(
-                                                                                   "inc",
-                                                                                   "D|DV|DD",
-                                                                                   "Increments a variable by 1 if no arguments specified."
-                                                                                  );
+                 "inc",
+                 "D|DV|DD",
+                 "Increments a variable by 1 if no arguments specified."
+                );
             PluginHost.AddInstructionWithDefaultCreator<MultiplyVarFLInstruction>(
-                                                                                  "multiply",
-                                                                                  "DV|DD",
-                                                                                  "Multiplies a variable by the arguments Specified."
-                                                                                 );
+                 "multiply",
+                 "DV|DD",
+                 "Multiplies a variable by the arguments Specified."
+                );
             PluginHost.AddInstructionWithDefaultCreator<DivideVarFLInstruction>(
                                                                                 "divide",
                                                                                 "DV|DD",
                                                                                 "Divides a variable by the arguments Specified."
                                                                                );
             PluginHost.AddInstructionWithDefaultCreator<BranchLessOrEqualFLInstruction>(
-                                                                                        "ble",
-                                                                                        "DVX|VVX|DDX",
-                                                                                        "Branches to the Specified function or script when firstparameter <= secondparameter"
-                                                                                       );
+                 "ble",
+                 "DVX|VVX|DDX",
+                 "Branches to the Specified function or script when firstparameter <= secondparameter"
+                );
             PluginHost.AddInstructionWithDefaultCreator<BranchGreaterOrEqualFLInstruction>(
-                                                                                           "bge",
-                                                                                           "DVX|VVX|DDX",
-                                                                                           "Branches to the Specified function or script when firstparameter >= secondparameter"
-                                                                                          );
+                 "bge",
+                 "DVX|VVX|DDX",
+                 "Branches to the Specified function or script when firstparameter >= secondparameter"
+                );
             PluginHost.AddInstructionWithDefaultCreator<BranchLessThanFLInstruction>(
-                                                                                     "blt",
-                                                                                     "DVX|VVX|DDX",
-                                                                                     "Branches to the Specified function or script when firstparameter < secondparameter"
-                                                                                    );
+                 "blt",
+                 "DVX|VVX|DDX",
+                 "Branches to the Specified function or script when firstparameter < secondparameter"
+                );
             PluginHost.AddInstructionWithDefaultCreator<BranchGreaterThanFLInstruction>(
-                                                                                        "bgt",
-                                                                                        "DVX|VVX|DDX",
-                                                                                        "Branches to the Specified function or script when firstparameter > secondparameter"
-                                                                                       );
+                 "bgt",
+                 "DVX|VVX|DDX",
+                 "Branches to the Specified function or script when firstparameter > secondparameter"
+                );
         }
 
     }
